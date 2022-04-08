7adb94158c63ca0f6cc3dd5b7a8606c4eb5f14c2
Hajin Kim <rhrnak624@naver.com>
2022-04-08 23:21:03 +0900

    [Node] 변수, 템플릿 리터럴 #2

---
+++ syntax/template.js
@@ -0,0 +1,6 @@
+var name = 'hajin'
+var letter = `Dear ${name},
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. ${name} Duis facilisis turpis nisl, hendrerit tincidunt nulla imperdiet eu. Aenean tristique massa in felis volutpat egestas. Sed feugiat sollicitudin urna in accumsan. Quisque feugiat, lacus ut pretium bibendum, eros tellus condimentum justo, ac tincidunt risus turpis et dolor. Aenean non tristique arcu, hendrerit rutrum nisi. Praesent fringilla dictum purus eu laoreet. Quisque ac scelerisque turpis, at scelerisque nibh. Ut ac sollicitudin nunc. Quisque a ipsum maximus, efficitur nunc at, rhoncus sem.
+`
+console.log(letter)
